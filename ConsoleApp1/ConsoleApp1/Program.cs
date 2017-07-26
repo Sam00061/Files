175c7c44e2298332cbc7d14d205babe380562024
Sam00061 <sam.jheng@skymirror.com.tw>
2017-07-26 11:13:53 +0800

    Example Code for garbled text

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.IO;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            System.Diagnostics.Process SubProcess = new System.Diagnostics.Process()
+            {
+                StartInfo = new System.Diagnostics.ProcessStartInfo()
+                {
+                    FileName = "C:\\Windows\\System32\\w32tm.exe",
+                    Arguments = "/config /manualpeerlist:\"time.stdtime.gov.tw clock.stdtime.gov.tw tick.stdtime.gov.tw watch.stdtime.gov.tw\" /syncfromflags:manual /reliable:yes /update",
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                }
+            };
+            SubProcess.Start();
+            SubProcess.WaitForExit();
+            File.WriteAllText("Output.txt", SubProcess.StandardError.ReadToEnd());
+            File.AppendAllText("Output.txt", SubProcess.StandardOutput.ReadToEnd());
+        }
+
+    }
+}
